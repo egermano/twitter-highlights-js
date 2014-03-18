43854a5b69c4911abf0440504e899f6c51b9c8ac
Bruno Germano <bruno.germano@gmail.com>
2014-03-18 15:14:52 +0000

    add jquery plugin

---
+++ jquery.twitterParsing.js
@@ -0,0 +1,88 @@
+
+/**
+ * @author Bruno Germano
+ * @file jquery.twitterParsing.js
+ * @date 18/03/2014 12:14
+ */
+
+
+/**
+ * Prototyping String,link
+ *
+ * @param string str A url to create a link.
+ * @param object opt Option param [bool nofollow, bool newWindow, string className]
+ */
+String.prototype.link = function(str,opt) {
+	aux = "";
+	if(!opt)
+		return "<a href='"+ str +"'>" + this + "</a>"
+	else {
+		if(opt.nofollow)
+			aux += " rel='nofollow' "
+		if(opt.newWindow)
+			aux += " target='_blank' "
+		if(opt.className)
+			aux += " class='" + opt.className + "' "
+
+		return "<a href='"+ str +"'" + aux + ">" + this + "</a>"
+	}
+}
+
+/**
+ * Prototyping String to create a parseURL method
+ *
+ * Method will create a links in url strings
+ *
+ * @param none
+ */
+String.prototype.parseURL = function() {
+	return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
+		var options = {nofollow:true, newWindow: true};
+		return url.link(url, options);
+	});
+};
+
+/**
+ * Prototyping String to create a parseUsername method
+ *
+ * Method will create a links in @username strings
+ *
+ * @param none
+ */
+String.prototype.parseUsername = function() {
+	return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+		var username = u.replace("@","")
+		var options = {nofollow:true, newWindow: true};
+		return u.link("http://twitter.com/"+username, options);
+	});
+};
+
+/**
+ * Prototyping String to create a parseHashtag method
+ *
+ * Method will create a links in #hashtags strings
+ *
+ * @param none
+ */
+String.prototype.parseHashtag = function() {
+	return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+		var tag = t.replace("#","%23")
+		var options = {nofollow:true, newWindow: true};
+		return t.link("http://search.twitter.com/search?q="+tag, options );
+	});
+};
+
+/**
+ * jquery Plugin to parse twitter messagens
+ * @param  object $ jQuery denpendency
+ * @return self element/object
+ */
+(function($){
+    $.fn.twitterParsing = function(){
+        return this.each(function(){
+            var $text = $(this).text();
+
+            $(this).text($text.parseURL().parseHashtag().parseUsername());
+        });
+    };
+}(jQuery));
--- twitter.js
+++ twitter.js
@@ -2,20 +2,20 @@
  * @author Bruno Germano
  */
 function buildTweet (tweet){
-	var from = document.createElement('div');
-	var msg = document.createElement('div');
-	var date = document.createElement('div');
+    var from = document.createElement('div');
+    var msg = document.createElement('div');
+    var date = document.createElement('div');
 
-	//from user
-	from.className = 'from';
-	from.innerHTML ="FROM @" + tweet.user.screen_name;
+    //from user
+    from.className = 'from';
+    from.innerHTML ="FROM @" + tweet.user.screen_name;
 
-	//message
-	msg.className = 'mensagem';
-	msg.innerHTML =  tweet.text;
+    //message
+    msg.className = 'mensagem';
+    msg.innerHTML =  tweet.text;
 
-	//date from menssage
-	var data = new Date(Date.parse(tweet.created_at)).toLocaleDateString();
+    //date from menssage
+    var data = new Date(Date.parse(tweet.created_at)).toLocaleDateString();
     var hora = new Date(Date.parse(tweet.created_at)).toLocaleTimeString();
     var tweetDate = new Date(data + ' ' + hora);
     var today = new Date();
@@ -24,33 +24,33 @@ function buildTweet (tweet){
     var dataText = '';
 
     if(diff<(1000*60*60*24)){
-    	if(diff<(1000*60*60)){
-    		var minutos = Math.floor(diff/(1000*60))
-    		if(minutos <= 1)
-    			dataText = ' now';
-    		else{
-    			dataText = minutos.toString() + ' minutes ago'
-    		}
-    	}else{
-    		var horas = Math.floor(diff/(1000*60*60))
-    		dataText = horas.toString() + (horas==1?' hour ago':' hours ago');
-    	}
+        if(diff<(1000*60*60)){
+            var minutos = Math.floor(diff/(1000*60))
+            if(minutos <= 1)
+                dataText = ' now';
+            else{
+                dataText = minutos.toString() + ' minutes ago'
+            }
+        }else{
+            var horas = Math.floor(diff/(1000*60*60))
+            dataText = horas.toString() + (horas==1?' hour ago':' hours ago');
+        }
     }else{
-    	var dias = Math.floor(diff/(1000*60*60*24))
-    	if(dias==1){
-    		dataText = ' yestarday';
-    	}else{
-    		dataText = dias.toString() + ' days ago';
-    	}
+        var dias = Math.floor(diff/(1000*60*60*24))
+        if(dias==1){
+            dataText = ' yestarday';
+        }else{
+            dataText = dias.toString() + ' days ago';
+        }
     }
     date.className = 'data';
     date.innerHTML = dataText;
 
-	var div = document.createElement('div');
+    var div = document.createElement('div');
 
-	div.appendChild(from);
-	div.appendChild(msg);
-	div.appendChild(date);
+    div.appendChild(from);
+    div.appendChild(msg);
+    div.appendChild(date);
 
-	return div.innerHTML;
+    return div.innerHTML;
 }
